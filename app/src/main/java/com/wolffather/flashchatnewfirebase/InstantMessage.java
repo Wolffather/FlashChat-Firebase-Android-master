2c07ceb939b54aa72c72e7a3e2cf33c469903a5e
Wolffather <maxcross93@gmail.com>
2017-12-22 15:51:09 +0300

    Initial commit

---
+++ app/src/main/java/com/wolffather/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,27 @@
+package com.wolffather.flashchatnewfirebase;
+
+/**
+ * Created by Admin on 09.12.2017.
+ */
+
+public class InstantMessage {
+
+    private String message;
+    private String author;
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
